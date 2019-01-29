95d9971d04a4c34352f623d9f60bc0397d3063db
kuhblume <kasumigaura0524@yahoo.co.jp>
2019-01-30 03:44:27 +0900

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+function convertP(add) {
+    var mm = document.getElementById(add).value;
+    document.getElementById("test").style.filter=add+'('+mm+'%)';
+}
+function convertD(add) {
+    var mm = document.getElementById(add).value;
+    document.getElementById("test").style.filter=add+'('+mm+'deg)';
+}
+function convertPx(add) {
+    var mm = document.getElementById(add).value;
+    document.getElementById("test").style.filter=add+'('+mm+'px)';
+}
