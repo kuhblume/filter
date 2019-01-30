05c00fec1b6d75604a177b97044723347f731ff4
kuhblume <kasumigaura0524@yahoo.co.jp>
2019-01-30 19:28:58 +0900

    見た目と操作性の向上

--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,46 @@
-function convertP(add) {
-    var mm = document.getElementById(add).value;
-    document.getElementById("test").style.filter+=add+'('+mm+'%)';
+function changeData(){
+    document.getElementById("test").style.filter=
+        'hue-rotate('+String(data.rotate)+'deg) '+
+        'brightness('+data.brightness+'%) '+
+        'contrast('+data.contrast+'%) '+
+        'grayscale('+data.grayscale+'%) '+
+        'saturate('+data.saturate+'%) '+
+        'sepia('+data.sepia+'%) '+
+        'invert('+data.invert+'%) '+
+        'opacity('+data.opacity+'%) '+
+        'blur('+data.blur+'px) ';
 }
-function convertD(add) {
-    var mm = document.getElementById(add).value;
-    document.getElementById("test").style.filter+=add+'('+mm+'deg)';
+
+
+let inputs = document.getElementById("inputList");
+function input(){
+    for (let i = 0; i < inputs.children.length; i++) {
+        if (inputs.children[i].matches('label')) {
+            data[inputs.children[i].children[1].id]=inputs.children[i].children[1].value;
+            inputs.children[i].children[0].innerHTML=inputs.children[i].children[1].id+':'+data[inputs.children[i].children[1].id];
+        }
+    }
+    changeData();
 }
-function convertPx(add) {
-    var mm = document.getElementById(add).value;
-    document.getElementById("test").style.filter+=add+'('+mm+'px)';
+
+function reset(){
+    data={
+        rotate:0,
+        brightness:100,
+        contrast:100,
+        grayscale:0,
+        saturate:100,
+        sepia:0,
+        invert:0,
+        opacity:100,
+        blur:0,
+    };
+    changeData();
+    for (let i = 0; i < inputs.children.length; i++) {
+        if (inputs.children[i].matches('label')) {
+            inputs.children[i].children[1].value=data[inputs.children[i].children[1].id];
+            inputs.children[i].children[0].innerHTML=inputs.children[i].children[1].id+':'+data[inputs.children[i].children[1].id];
+        }
+    }
 }
+reset();//init
